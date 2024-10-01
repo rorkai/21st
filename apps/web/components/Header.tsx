312efd7ccaaa1427014b76c68cf939e3a60f00c2
serafim.eth <serafimcloud@gmail.com>
2024-10-01 20:34:02 +0300

    Implement Publish Button in Header

--- apps/web/components/Header.tsx
+++ apps/web/components/Header.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { LockOpen, Lock } from 'lucide-react';
+import { Button } from "@/components/ui/button";
 
 interface HeaderProps {
   componentSlug?: string;
@@ -15,33 +16,39 @@ export function Header({ componentSlug, isPublic }: HeaderProps) {
   const isHomePage = pathname === '/';
 
   return (
-    <header className="flex items-center gap-4">
-      <div className="flex items-center">
-        <Link href="/" className="flex items-center">
-          <Image src="/cc-logo.svg" alt="Logo" width={32} height={32} />
-        </Link>
-        {!isHomePage && componentSlug && (
-          <>
-            <span className="mx-2 text-gray-400">/</span>
-            <span className="text-gray-700">{componentSlug}</span>
-          </>
+    <header className="flex items-center justify-between">
+      <div className="flex items-center gap-4">
+        <div className="flex items-center">
+          <Link href="/" className="flex items-center">
+            <Image src="/cc-logo.svg" alt="Logo" width={32} height={32} />
+          </Link>
+          {!isHomePage && componentSlug && (
+            <>
+              <span className="mx-2 text-gray-400">/</span>
+              <span className="text-gray-700">{componentSlug}</span>
+            </>
+          )}
+        </div>
+        {!isHomePage && (
+          <div className="text-[14px] border border-gray-200 rounded-full px-2 py-[1px]">
+            {isPublic ? (
+              <div className="flex gap-2 items-center">
+                  <LockOpen className="w-3 h-3" />
+                <span>Public</span>
+              </div>
+            ) : (
+              <div className="flex gap-2 items-center">
+                  <Lock className="w-3 h-3" />
+                <span>Private</span>
+              </div>
+            )}
+          </div>
         )}
       </div>
       {!isHomePage && (
-        <div className="text-[14px] border border-gray-200 rounded-full px-2 py-[1px]">
-          {isPublic ? (
-            <div className="flex gap-2 items-center">
-                <LockOpen className="w-3 h-3" />
-              <span>Public</span>
-              
-            </div>
-          ) : (
-            <div className="flex gap-2 items-center">
-                <Lock className="w-3 h-3" />
-              <span>Private</span>
-            </div>
-          )}
-        </div>
+        <Button asChild>
+          <Link href="/publish">Publish</Link>
+        </Button>
       )}
     </header>
   );

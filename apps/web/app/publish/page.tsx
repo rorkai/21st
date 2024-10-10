b28fae4ef13c8d21b59f20efcdc3037355b1760f
serafim.eth <serafimcloud@gmail.com>
2024-10-10 11:42:48 +0300

    Add back

--- apps/web/app/publish/page.tsx
+++ apps/web/app/publish/page.tsx
@@ -7,6 +7,7 @@ import ComponentForm from "@/components/ComponentForm/ComponentForm"
 import { LoadingSpinner } from "@/components/Loading"
 import { Header } from "@/components/Header"
 import Head from "next/head"
+import { House } from "lucide-react"
 
 export default function PublishPage() {
   const { isLoaded, isSignedIn } = useUser()
@@ -28,6 +29,12 @@ export default function PublishPage() {
         <title>Publish New Component | Component Library</title>
       </Head>
       <SignedIn>
+        <a
+          href="/"
+          className="absolute top-4 left-4 cursor-pointer hover:bg-gray-100 rounded-md h-8 w-8 flex items-center justify-center"
+        >
+          <House size={18} />
+        </a>
         <div className="flex flex-col items-center gap-7 h-screen w-full">
           <ComponentForm />
         </div>

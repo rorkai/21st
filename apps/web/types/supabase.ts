0c581f8789d4d28cdacdd197d4913567a063aa2b
shad <shadowagent1st@gmail.com>
2024-10-16 11:51:31 +0300

    refactor: added supabase types

--- apps/web/types/supabase.ts
+++ apps/web/types/supabase.ts
@@ -1,45 +1,384 @@
-import { User, Component } from "./types"
-
-export interface ComponentOwnerData {
-  user: {
-    username: string
-  }
-}
-
-export type GenericView = {
-  Row: Record<string, unknown>
-}
-
-export type GenericFunction = {
-  Args: Record<string, unknown>
-  Returns: unknown
-}
+export type Json =
+  | string
+  | number
+  | boolean
+  | null
+  | { [key: string]: Json | undefined }
+  | Json[]
 
 export type Database = {
   public: {
     Tables: {
+      component_likes: {
+        Row: {
+          component_id: number
+          liked_at: string | null
+          user_id: string
+        }
+        Insert: {
+          component_id: number
+          liked_at?: string | null
+          user_id: string
+        }
+        Update: {
+          component_id?: number
+          liked_at?: string | null
+          user_id?: string
+        }
+        Relationships: [
+          {
+            foreignKeyName: "component_likes_component_id_fkey"
+            columns: ["component_id"]
+            isOneToOne: false
+            referencedRelation: "components"
+            referencedColumns: ["id"]
+          },
+          {
+            foreignKeyName: "component_likes_user_id_fkey"
+            columns: ["user_id"]
+            isOneToOne: false
+            referencedRelation: "users"
+            referencedColumns: ["id"]
+          },
+        ]
+      }
+      component_tags: {
+        Row: {
+          component_id: number
+          tag_id: number
+        }
+        Insert: {
+          component_id?: number
+          tag_id: number
+        }
+        Update: {
+          component_id?: number
+          tag_id?: number
+        }
+        Relationships: [
+          {
+            foreignKeyName: "component_tags_component_id_fkey"
+            columns: ["component_id"]
+            isOneToOne: false
+            referencedRelation: "components"
+            referencedColumns: ["id"]
+          },
+          {
+            foreignKeyName: "component_tags_tag_id_fkey"
+            columns: ["tag_id"]
+            isOneToOne: false
+            referencedRelation: "tags"
+            referencedColumns: ["id"]
+          },
+        ]
+      }
       components: {
-        Row: Component
-        Insert: Omit<
-          Component,
-          | "id"
-          | "created_at"
-          | "updated_at"
-          | "user"
-          | "downloads_count"
-          | "likes_count"
-        >
-        Update: Partial<
-          Omit<Component, "id" | "created_at" | "updated_at" | "user">
-        >
+        Row: {
+          code: string
+          component_name: Json
+          component_slug: string
+          created_at: string
+          demo_code: string
+          demo_component_name: string
+          demo_component_names: Json | null
+          demo_dependencies: Json | null
+          dependencies: Json | null
+          description: string | null
+          downloads_count: number
+          fts: unknown | null
+          id: number
+          internal_dependencies: Json | null
+          is_public: boolean
+          license: string
+          likes_count: number
+          name: string
+          preview_url: string
+          updated_at: string
+          user_id: string
+        }
+        Insert: {
+          code?: string
+          component_name: Json
+          component_slug: string
+          created_at?: string
+          demo_code: string
+          demo_component_name: string
+          demo_component_names?: Json | null
+          demo_dependencies?: Json | null
+          dependencies?: Json | null
+          description?: string | null
+          downloads_count?: number
+          fts?: unknown | null
+          id?: number
+          internal_dependencies?: Json | null
+          is_public?: boolean
+          license?: string
+          likes_count?: number
+          name: string
+          preview_url: string
+          updated_at?: string
+          user_id: string
+        }
+        Update: {
+          code?: string
+          component_name?: Json
+          component_slug?: string
+          created_at?: string
+          demo_code?: string
+          demo_component_name?: string
+          demo_component_names?: Json | null
+          demo_dependencies?: Json | null
+          dependencies?: Json | null
+          description?: string | null
+          downloads_count?: number
+          fts?: unknown | null
+          id?: number
+          internal_dependencies?: Json | null
+          is_public?: boolean
+          license?: string
+          likes_count?: number
+          name?: string
+          preview_url?: string
+          updated_at?: string
+          user_id?: string
+        }
+        Relationships: [
+          {
+            foreignKeyName: "components_user_id_fkey"
+            columns: ["user_id"]
+            isOneToOne: false
+            referencedRelation: "users"
+            referencedColumns: ["id"]
+          },
+        ]
+      }
+      tags: {
+        Row: {
+          id: number
+          name: string
+          slug: string
+        }
+        Insert: {
+          id?: number
+          name: string
+          slug: string
+        }
+        Update: {
+          id?: number
+          name?: string
+          slug?: string
+        }
+        Relationships: []
       }
       users: {
-        Row: User
-        Insert: Omit<User, "id" | "created_at" | "updated_at">
-        Update: Partial<Omit<User, "id" | "created_at" | "updated_at">>
+        Row: {
+          created_at: string
+          email: string | null
+          id: string
+          image_url: string | null
+          manually_added: boolean
+          name: string | null
+          updated_at: string | null
+          username: string
+        }
+        Insert: {
+          created_at?: string
+          email?: string | null
+          id: string
+          image_url?: string | null
+          manually_added?: boolean
+          name?: string | null
+          updated_at?: string | null
+          username: string
+        }
+        Update: {
+          created_at?: string
+          email?: string | null
+          id?: string
+          image_url?: string | null
+          manually_added?: boolean
+          name?: string | null
+          updated_at?: string | null
+          username?: string
+        }
+        Relationships: []
       }
     }
-    Views: Record<string, GenericView>
-    Functions: Record<string, GenericFunction>
+    Views: {
+      [_ in never]: never
+    }
+    Functions: {
+      delete_component: {
+        Args: {
+          component_id: number
+        }
+        Returns: boolean
+      }
+      get_component_by_slug: {
+        Args: {
+          user_username: string
+          component_slug_param: string
+        }
+        Returns: {
+          component_id: number
+          component_name: Json
+          description: string
+          code: string
+          demo_code: string
+          created_at: string
+          updated_at: string
+          user_id: string
+          install_url: string
+          dependencies: Json
+          is_public: boolean
+          downloads_count: number
+          likes_count: number
+          component_slug: string
+          demo_component_name: string
+          name: string
+          demo_dependencies: Json
+          internal_dependencies: Json
+          preview_url: string
+          license: string
+          user_data: Json
+          tags: Json
+        }[]
+      }
+      requesting_user_id: {
+        Args: Record<PropertyKey, never>
+        Returns: string
+      }
+      search_components: {
+        Args: {
+          search_query: string
+        }
+        Returns: {
+          component_id: number
+          component_name: Json
+          description: string
+          code: string
+          demo_code: string
+          created_at: string
+          updated_at: string
+          user_id: string
+          install_url: string
+          dependencies: Json
+          is_public: boolean
+          downloads_count: number
+          likes_count: number
+          component_slug: string
+          demo_component_name: string
+          name: string
+          demo_dependencies: Json
+          internal_dependencies: Json
+          preview_url: string
+          license: string
+          user_data: Json
+        }[]
+      }
+    }
+    Enums: {
+      [_ in never]: never
+    }
+    CompositeTypes: {
+      [_ in never]: never
+    }
   }
 }
+
+type PublicSchema = Database[Extract<keyof Database, "public">]
+
+export type Tables<
+  PublicTableNameOrOptions extends
+    | keyof (PublicSchema["Tables"] & PublicSchema["Views"])
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
+        Database[PublicTableNameOrOptions["schema"]]["Views"])
+    : never = never,
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
+      Database[PublicTableNameOrOptions["schema"]]["Views"])[TableName] extends {
+      Row: infer R
+    }
+    ? R
+    : never
+  : PublicTableNameOrOptions extends keyof (PublicSchema["Tables"] &
+        PublicSchema["Views"])
+    ? (PublicSchema["Tables"] &
+        PublicSchema["Views"])[PublicTableNameOrOptions] extends {
+        Row: infer R
+      }
+      ? R
+      : never
+    : never
+
+export type TablesInsert<
+  PublicTableNameOrOptions extends
+    | keyof PublicSchema["Tables"]
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
+    : never = never,
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+      Insert: infer I
+    }
+    ? I
+    : never
+  : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
+    ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
+        Insert: infer I
+      }
+      ? I
+      : never
+    : never
+
+export type TablesUpdate<
+  PublicTableNameOrOptions extends
+    | keyof PublicSchema["Tables"]
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
+    : never = never,
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+      Update: infer U
+    }
+    ? U
+    : never
+  : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
+    ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
+        Update: infer U
+      }
+      ? U
+      : never
+    : never
+
+export type Enums<
+  PublicEnumNameOrOptions extends
+    | keyof PublicSchema["Enums"]
+    | { schema: keyof Database },
+  EnumName extends PublicEnumNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicEnumNameOrOptions["schema"]]["Enums"]
+    : never = never,
+> = PublicEnumNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicEnumNameOrOptions["schema"]]["Enums"][EnumName]
+  : PublicEnumNameOrOptions extends keyof PublicSchema["Enums"]
+    ? PublicSchema["Enums"][PublicEnumNameOrOptions]
+    : never
+
+export type CompositeTypes<
+  PublicCompositeTypeNameOrOptions extends
+    | keyof PublicSchema["CompositeTypes"]
+    | { schema: keyof Database },
+  CompositeTypeName extends PublicCompositeTypeNameOrOptions extends {
+    schema: keyof Database
+  }
+    ? keyof Database[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"]
+    : never = never,
+> = PublicCompositeTypeNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"][CompositeTypeName]
+  : PublicCompositeTypeNameOrOptions extends keyof PublicSchema["CompositeTypes"]
+    ? PublicSchema["CompositeTypes"][PublicCompositeTypeNameOrOptions]
+    : never

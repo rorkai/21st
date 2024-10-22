b6692e64e838d122a6a77f64a5599bcd4536b058
Daniel Dhawan <shadowagent1st@gmail.com>
2024-10-22 20:37:57 +0300

    fix loading spinner

--- apps/web/components/LoadingSpinner.tsx
+++ apps/web/components/LoadingSpinner.tsx
@@ -1,7 +1,7 @@
 import { Loader2 } from "lucide-react"
 
 export const LoadingSpinner = () => (
-  <div className="absolute inset-0 flex items-center justify-center bg-background">
+  <div className="w-full h-full flex items-center justify-center bg-background">
     <Loader2 className="w-5 h-5 text-foreground animate-spin" />
     <span className="ml-2 text-foreground">Loading...</span>
   </div>

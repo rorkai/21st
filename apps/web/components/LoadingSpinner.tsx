18d9d2e5939b900026d56a853ee048c75825443a
Daniel Dhawan <shadowagent1st@gmail.com>
2024-10-24 14:20:07 +0300

    fix: loading spinner

--- apps/web/components/LoadingSpinner.tsx
+++ apps/web/components/LoadingSpinner.tsx
@@ -1,5 +1,5 @@
 export const LoadingSpinner = () => (
-  <div className="absolute inset-0 flex items-center justify-center bg-background">
+  <div className="w-full h-full flex items-center justify-center bg-background">
     <div className="relative w-4 h-4">
       <div className="absolute inset-0 bg-foreground/30 rounded-full animate-pulse-slow"></div>
       <div className="absolute inset-0 bg-foreground rounded-full animate-pulse-fast"></div>

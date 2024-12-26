1d650f118425efcc4d941e5d98c4a22108f53c90
Daniel Dhawan <shadowagent1st@gmail.com>
2024-12-25 22:14:39 -0800

    fix: header on publish page

--- apps/web/app/publish/page.tsx
+++ apps/web/app/publish/page.tsx
@@ -3,7 +3,7 @@ import { SignedIn, SignedOut, RedirectToSignIn } from "@clerk/nextjs"
 import PublishComponentForm from "@/components/publish/PublishComponentForm"
 
 import Head from "next/head"
-import Link from "next/link"
+import { Header } from "@/components/Header"
 
 export default function PublishPage() {
   return (
@@ -12,12 +12,7 @@ export default function PublishPage() {
         <title>Publish New Component | Component Library</title>
       </Head>
       <SignedIn>
-        <Link
-          href="/"
-          className="absolute top-4 left-4 cursor-pointer w-5 h-5 flex items-center justify-center"
-        >
-          <div className="w-full h-full z-10 rounded-full bg-foreground" />
-        </Link>
+        <Header page="publish" />
         <div className="flex flex-row items-center h-screen w-full py-4">
           <PublishComponentForm />
         </div>

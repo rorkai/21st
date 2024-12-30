120906fa411b983e22af75f8ba83bf9427cd7088
serafim.eth <serafimcloud@gmail.com>
2024-12-31 00:45:56 +0300

    fix: Info → Details

--- apps/web/components/ComponentPage.tsx
+++ apps/web/components/ComponentPage.tsx
@@ -505,12 +505,12 @@ export default function ComponentPage({
                     }`}
                   >
                     <Info size={18} />
-                    <span className="pl-1 pr-2 text-[14px]">Info</span>
+                    <span className="pl-1 pr-2 text-[14px]">Details</span>
                   </button>
                 </TooltipTrigger>
                 <TooltipContent className="z-50 overflow-hidden rounded-md border bg-popover px-3 py-1.5 text-sm text-popover-foreground shadow-md animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2">
                   <p className="flex items-center">
-                    Component info
+                    Component details
                     <Hotkey keys={["]"]} variant="outline" />
                   </p>
                 </TooltipContent>

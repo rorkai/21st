064d7a99433d3f4cc20e463a116efd82689ca8c1
Daniel Dhawan <shadowagent1st@gmail.com>
2024-12-30 05:47:52 +0300

    fix: theme toggle

--- apps/web/components/ThemeToggle.tsx
+++ apps/web/components/ThemeToggle.tsx
@@ -5,27 +5,25 @@ import { Moon, Sun } from "lucide-react"
 import { Toggle } from "@/components/ui/toggle"
 
 export function ThemeToggle() {
-  // we are not using { resolvedTheme } here
-  // to display pressed state when the theme is different from the system theme
-  const { theme, setTheme } = useTheme()
+  const { resolvedTheme, setTheme } = useTheme()
 
   return (
     <Toggle
       className="group h-8 w-8 bg-transparent data-[state=on]:!bg-transparent hover:bg-accent hover:!text-foreground"
-      pressed={theme === "dark"}
-      onPressedChange={() => setTheme(theme === "dark" ? "light" : "dark")}
-      aria-label={`Switch to ${theme === "dark" ? "light" : "dark"} mode`}
+      pressed={resolvedTheme === "dark"}
+      onPressedChange={() => setTheme(resolvedTheme === "dark" ? "light" : "dark")}
+      aria-label={`Switch to ${resolvedTheme === "dark" ? "light" : "dark"} mode`}
     >
       <Moon
         size={16}
         strokeWidth={2}
-        className="shrink-0 scale-0 opacity-0 transition-all group-data-[state=on]:scale-100 group-data-[state=on]:opacity-100"
+        className="shrink-0 scale-100 opacity-100 transition-all group-data-[state=on]:scale-0 group-data-[state=on]:opacity-0"
         aria-hidden="true"
       />
       <Sun
         size={16}
         strokeWidth={2}
-        className="absolute shrink-0 scale-100 opacity-100 transition-all group-data-[state=on]:scale-0 group-data-[state=on]:opacity-0"
+        className="absolute shrink-0 scale-0 opacity-0 transition-all group-data-[state=on]:scale-100 group-data-[state=on]:opacity-100"
         aria-hidden="true"
       />
     </Toggle>

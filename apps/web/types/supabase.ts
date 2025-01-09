7ac42f81c5e947a10ec4520510cf2b73f5afad61
Daniel Dhawan <shadowagent1st@gmail.com>
2025-01-08 18:15:28 -0800

    chore: upd supabase types

--- apps/web/types/supabase.ts
+++ apps/web/types/supabase.ts
@@ -7,31 +7,6 @@ export type Json =
   | Json[]
 
 export type Database = {
-  graphql_public: {
-    Tables: {
-      [_ in never]: never
-    }
-    Views: {
-      [_ in never]: never
-    }
-    Functions: {
-      graphql: {
-        Args: {
-          operationName?: string
-          query?: string
-          variables?: Json
-          extensions?: Json
-        }
-        Returns: Json
-      }
-    }
-    Enums: {
-      [_ in never]: never
-    }
-    CompositeTypes: {
-      [_ in never]: never
-    }
-  }
   public: {
     Tables: {
       component_dependencies_closure: {

3d2c2fad972f9f0c67a1ce72bf2807d19d68c287
Daniel Dhawan <shadowagent1st@gmail.com>
2025-01-11 00:59:35 -0800

    fix: tailwind config after merge

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -116,37 +116,35 @@ module.exports = {
             height: "0",
           },
         },
-  			aurora: {
-  				from: {
-  					backgroundPosition: '50% 50%, 50% 50%'
-  				},
-  				to: {
-  					backgroundPosition: '350% 50%, 350% 50%'
-  				}
-  			},
-  			'spin-around': {
-  				'0%': {
-  					transform: 'translateZ(0) rotate(0)'
-  				},
-  				'15%, 35%': {
-  					transform: 'translateZ(0) rotate(90deg)'
-  				},
-  				'65%, 85%': {
-  					transform: 'translateZ(0) rotate(270deg)'
-  				},
-  				'100%': {
-  					transform: 'translateZ(0) rotate(360deg)'
-  				},
-  				'shimmer-slide': {
-  					to: {
-  						transform: 'translate(calc(100cqw - 100%), 0)'
-  					}
-  				}
-  			}
+        "spin-around": {
+          "0%": {
+            transform: "translateZ(0) rotate(0)",
+          },
+          "15%, 35%": {
+            transform: "translateZ(0) rotate(90deg)",
+          },
+          "65%, 85%": {
+            transform: "translateZ(0) rotate(270deg)",
+          },
+          "100%": {
+            transform: "translateZ(0) rotate(360deg)",
+          },
+        },
+        "shimmer-slide": {
+          to: {
+            transform: "translate(calc(100cqw - 100%), 0)",
+          },
+        },
         "success-pulse": {
-          "0%": { opacity: 0 },
-          "50%": { opacity: 1 },
-          "100%": { opacity: 0 },
+          "0%": {
+            opacity: 0,
+          },
+          "50%": {
+            opacity: 1,
+          },
+          "100%": {
+            opacity: 0,
+          },
         },
         "success-ring": {
           "0%": {
@@ -181,9 +179,10 @@ module.exports = {
         "pulse-slow": "pulse-custom 2s cubic-bezier(0.4, 0, 0.6, 1) infinite",
         "pulse-fast":
           "pulse-custom 2s cubic-bezier(0.4, 0, 0.6, 1) infinite 1s",
-  			aurora: 'aurora 60s linear infinite',
-  			'shimmer-slide': 'shimmer-slide var(--speed) ease-in-out infinite alternate',
-  			'spin-around': 'spin-around calc(var(--speed) * 2) infinite linear'
+        aurora: "aurora 60s linear infinite",
+        "shimmer-slide":
+          "shimmer-slide var(--speed) ease-in-out infinite alternate",
+        "spin-around": "spin-around calc(var(--speed) * 2) infinite linear",
         "success-ring": "success-ring 850ms ease-out forwards",
         "copy-success": "copy-success 1000ms ease-out forwards",
         "ping-slow": "ping-slow 2s cubic-bezier(0.4, 0, 0.6, 1) infinite",

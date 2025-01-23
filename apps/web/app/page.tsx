ea4aab948c389be8d4c725ca82214286855e9353
serafim.eth <serafimcloud@gmail.com>
2025-01-23 20:33:05 +0300

    hotfix

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -79,7 +79,7 @@ export const generateMetadata = async (): Promise<Metadata> => {
 export default async function HomePage() {
   const { userId } = auth()
   if (!userId) {
-    redirect("https://accounts.21st.dev/sign-in")
+    redirect("/sign-in")
   }
 
   const supabase = createClient(
